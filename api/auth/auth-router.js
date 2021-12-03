540ecb1cf23cdbd7d1b82deff7ea6dd18a04b5b5
bus42 <gbrewton1980@gmail.com>
2021-12-02 17:30:09 -0700

    registers user, returns hash

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const users = require('../users/users-model');
+const Users = require('../users/users-model');
 const { checkUsernameFree, checkPasswordLength, checkUsernameExists } = require('../auth/auth-middleware');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', checkUsernameFree, checkPasswordLength, (req, res) => {
-  users.add(req.body)
-    .then(user => {
-      if (user) { res.status(201).send(user) }
-    })
-    .catch(error => {
-      res.status(422).json({ message: error.message });
-    });
+  const { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 10);
+  Users.add({ username, password: hash })
+    .then(saved => res.status(201).send(saved))
+    .catch(error => res.status(500).json(error));
 });
 
 router.post('/login', checkUsernameExists, (req, res) => {
-  users.login(req.body)
+  Users.login(req.body)
     .then(response => res.status(200).send(response))
 })
 
 router.post('/logout', (req, res) => {
-  users.logout(req.body)
+  Users.logout(req.body)
     .then(user => res.status(200).send(user))
     .catch(error => res.status(422).json({ message: error.message }));
 })
